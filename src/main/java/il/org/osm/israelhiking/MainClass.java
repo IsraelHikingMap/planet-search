7ddb50bb3c888dbf92e8a09c87aea378d659df30
HarelM <harel.mazor@gmail.com>
2025-01-29 09:27:50 +0200

    Fixes #17 - Do no hang when there's a runtime error

--- src/main/java/il/org/osm/israelhiking/MainClass.java
+++ src/main/java/il/org/osm/israelhiking/MainClass.java
@@ -24,26 +24,28 @@ public class MainClass {
         
         var esAddress = args.getString("es-address", "Elasticsearch address", "http://localhost:9200");
         var esClient = ElasticsearchHelper.createElasticsearchClient(esAddress);
+        try {
+            var pointsIndexAlias = args.getString("es-points-index-alias", "Elasticsearch index to populate points", "points");
+            var bboxIndexAlias = args.getString("es-bbox-index-alias", "Elasticsearch index to populate bounding boxes", "bbox");
+            var supportedLanguages = args.getString("languages", "Languages to support", "en,he").split(",");
+            var externalFilePath = Path.of(args.getString("external-file-path", "Extranl file path", "data/sources/external.geojson"));
+            var targetPointsIndex = ElasticsearchHelper.createPointsIndex(esClient, pointsIndexAlias, supportedLanguages);
+            var targetBBoxIndex = ElasticsearchHelper.createBBoxIndex(esClient, bboxIndexAlias, supportedLanguages);
+            var profile = new PlanetSearchProfile(planetiler.config(), esClient, targetPointsIndex, targetBBoxIndex, supportedLanguages);
 
-        var pointsIndexAlias = args.getString("es-points-index-alias", "Elasticsearch index to populate points", "points");
-        var bboxIndexAlias = args.getString("es-bbox-index-alias", "Elasticsearch index to populate bounding boxes", "bbox");
-        var supportedLanguages = args.getString("languages", "Languages to support", "en,he").split(",");
-        var externalFilePath = Path.of(args.getString("external-file-path", "Extranl file path", "data/sources/external.geojson"));
-        var targetPointsIndex = ElasticsearchHelper.createPointsIndex(esClient, pointsIndexAlias, supportedLanguages);
-        var targetBBoxIndex = ElasticsearchHelper.createBBoxIndex(esClient, bboxIndexAlias, supportedLanguages);
-        var profile = new PlanetSearchProfile(planetiler.config(), esClient, targetPointsIndex, targetBBoxIndex, supportedLanguages);
+            String area = args.getString("area", "geofabrik area to download", "israel-and-palestine");
+            planetiler.setProfile(profile)
+            // override this default with osm_path="path/to/data.osm.pbf"
+            .addOsmSource("osm", Path.of("data", "sources", area + ".osm.pbf"), "geofabrik:" + area)
+            .addGeoJsonSource("external", externalFilePath)
+            // override this default with mbtiles="path/to/output.mbtiles"
+            .overwriteOutput(Path.of("data", "target", PlanetSearchProfile.POINTS_LAYER_NAME + ".pmtiles"))
+            .run();
 
-        String area = args.getString("area", "geofabrik area to download", "israel-and-palestine");
-        planetiler.setProfile(profile)
-          // override this default with osm_path="path/to/data.osm.pbf"
-          .addOsmSource("osm", Path.of("data", "sources", area + ".osm.pbf"), "geofabrik:" + area)
-          .addGeoJsonSource("external", externalFilePath)
-          // override this default with mbtiles="path/to/output.mbtiles"
-          .overwriteOutput(Path.of("data", "target", PlanetSearchProfile.POINTS_LAYER_NAME + ".pmtiles"))
-          .run();
-
-        ElasticsearchHelper.switchAlias(esClient, pointsIndexAlias, targetPointsIndex);
-        ElasticsearchHelper.switchAlias(esClient, bboxIndexAlias, targetBBoxIndex);
-        esClient.close();
+            ElasticsearchHelper.switchAlias(esClient, pointsIndexAlias, targetPointsIndex);
+            ElasticsearchHelper.switchAlias(esClient, bboxIndexAlias, targetBBoxIndex);
+        } finally {
+            esClient.close();
+        }
     }
 }
