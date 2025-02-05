4aeffd3fdf7e19af52b47badcb2032e18d5d0aca
HarelM <harel.mazor@gmail.com>
2025-02-05 23:41:49 +0200

    Make test a bit more complex for better coverage.

--- src/test/java/il/org/osm/israelhiking/E2ETest.java
+++ src/test/java/il/org/osm/israelhiking/E2ETest.java
@@ -8,5 +8,7 @@ public class E2ETest {
     @Test
     public void test() throws Exception {
         MainClass.main(new String[]{ "--download", "--external-file-path", "./src/test/resources/extenal.geojson"});
+        // Second run should not fail and also should not download the file again
+        MainClass.main(new String[]{ "--external-file-path", "./src/test/resources/extenal.geojson"});
     }
 }
